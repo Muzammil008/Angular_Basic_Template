ab7e71c850addd7cd912826772800c56b3bbc0c2
Muzammil <muzammilahmed973@gmail.com>
2018-04-23 17:01:48 +0500

    add forms in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 
 @NgModule({
@@ -13,17 +16,20 @@ import { ContactComponent } from './contact/contact.component';
     AppComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent
+    ContactComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    
     RouterModule.forRoot([
 
       { path: 'Home', component: HomeComponent },
       { path: '', redirectTo: '/Home', pathMatch: 'full' },
       { path: 'About', component: AboutComponent },
       { path: 'Contact', component: ContactComponent },
-
+      { path: 'Registration', component: RegistrationComponent },
     ])
   ],
   providers: [],
