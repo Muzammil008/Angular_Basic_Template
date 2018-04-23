9d1da647cf94eae8d4166354e1c082e25468f98e
Muzammil <muzammilahmed973@gmail.com>
2018-04-23 16:19:09 +0500

    Upload AngularAppTemplate on Github

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+
+      { path: 'Home', component: HomeComponent },
+      { path: '', redirectTo: '/Home', pathMatch: 'full' },
+      { path: 'About', component: AboutComponent },
+      { path: 'Contact', component: ContactComponent },
+
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
